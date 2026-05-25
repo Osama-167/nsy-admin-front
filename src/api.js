4dc3fa0b41fcfcc2e34acda82db22c5b52e657af
Osama-167 <107405759+Osama-167@users.noreply.github.com>
2026-05-25 21:14:34 +0300

    Update api.js

--- src/api.js
+++ src/api.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const API = axios.create({
-  baseURL: "nsybckend-production.up.railway.app",
+  baseURL: "nsybckend.railway.internal",
 });
 
 // =====================
